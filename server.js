4b76b4ae88e3155f89a3d7419d081d499cfab217
patpetpalopet <bodin.d@psru.ac.th>
2019-03-22 12:48:10 +0700

    customers

--- server.js
+++ server.js
@@ -1,8 +1,116 @@
 var http = require('http');
 var express = require('express');
-http.createServer(function (req, res) {
-    
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('dssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssssss');
-    
-}).listen(process.env.PORT || 8080);
+var sql = require('mssql');
+var bodyParser = require('body-parser');
+var request = require('request');
+var enrollment_status = 'Active';
+var app = express();
+var sqlConfig = {
+    user: 'metro',
+    password: 'P@ssw0rd',
+    server: 'mslazureconsumption.database.windows.net',
+    database: 'AzureConsumption',
+    options: {
+        encrypt: true
+    },
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 1
+    }
+};
+
+// set bodyParser
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded());
+
+// set Origin
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, X-access-token");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, POST");
+    res.header("Access-Control-Allow-Credentials", true);
+    next();
+});
+
+app.get('/customers', (req, res) => {
+    var connection = new sql.ConnectionPool(sqlConfig);
+    connection.connect().then(function () {
+        var request = new sql.Request(connection);
+        request.query(`select * from dbo.Customers`, function (erre, recordset) {
+            if (erre) {
+                console.log('ERROR: ', erre);
+                connection.close();
+            } else {
+                res.json({
+                    status: 200,
+                    recordset: recordset['recordset'],
+                    rows: recordset['rowsAffected']
+                });
+                connection.close();
+            }
+        });
+    });
+});
+
+app.get('/customers/:id', (req, res) => {
+    var connection = new sql.ConnectionPool(sqlConfig);
+    connection.connect().then(function () {
+        var request = new sql.Request(connection);
+        request.query(`select * from dbo.Customers where id=${req.params.id}`, function (erre, recordset) {
+            if (erre) {
+                console.log('ERROR: ', erre);
+                connection.close();
+            } else {
+                // console.log(recordset);
+                res.json({
+                    status: 200,
+                    recordset: recordset['recordset'][0]
+                });
+                connection.close();
+            }
+        });
+    });
+});
+
+app.get('/consumption/:enrollment_id', (req, res) => {
+    var connection = new sql.ConnectionPool(sqlConfig);
+    connection.connect().then(function () {
+        var request = new sql.Request(connection);
+        request.query(`select * from dbo.[${req.params.enrollment_id}]`, function (erre, recordset) {
+            if (erre) {
+                console.log('ERROR: ', erre);
+                connection.close();
+            } else {
+                res.json({
+                    status: 200,
+                    recordset: recordset['recordset'],
+                    rows: recordset['rowsAffected']
+                });
+                connection.close();
+            }
+        });
+    });
+});
+
+app.get('/dept/:id', (req, res) => {
+    var connection = new sql.ConnectionPool(sqlConfig);
+    connection.connect().then(function () {
+        var request = new sql.Request(connection);
+        request.query(`select * from dbo.dept where deptno=${req.params.id}`, function (erre, recordset) {
+            if (erre) {
+                console.log('ERROR: ', erre);
+                connection.close();
+            } else {
+                // console.log(recordset);
+                res.json(recordset['recordset']);
+                connection.close();
+            }
+        });
+    });
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log('Server running at http://localhost:' + port);
+});

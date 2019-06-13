41fb552453e1e813808bdff9ef774e6e42d5bcec
patpetpalopet <bodin.d@psru.ac.th>
2019-06-13 11:05:07 +0700

    sssssss

--- server.js
+++ server.js
@@ -308,7 +308,7 @@ function getAllData(_urllink, _tokeninput, _tableInsert, _markup) {
             var consuc = ((_markup * result[i].cost) / 100) + result[i].cost;
             var countfo = precision(consuc);
             if (countfo > 16) {
-                sendLog(`cost = ${consuc}  --- id =  ${result[i].id}  `);
+                sendLog(`cost = ${consuc}  --- id =  ${result[i].meterId}  `);
             }
             result[i].consumption_cost = consuc;
         }
